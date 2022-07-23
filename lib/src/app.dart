18395ae3f55bdc261de992d4919d29ecb809a2fe
Akshay Patel <akshay.patel@swymcorp.com>
2022-07-23 10:12:56 +0530

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello"),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add), onPressed: () => print("hello")),
+      ),
+    );
+  }
+}
